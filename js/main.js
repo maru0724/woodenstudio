8816023295818731718d7d6c73627beeb33db623
maru0724 <kangawac@gmail.com>
2025-06-25 15:28:53 +0900

    Upload Files
    公開用のファイルをアップロードしました

---
+++ js/main.js
@@ -0,0 +1,120 @@
+// GSAPからmqの関数を実行する
+const mm = gsap.matchMedia()
+// ブレイクポイントを設定
+// 端末の画面幅が1025px以上の時(pc)
+mm.add("(min-width: 1025px)",()=> {
+    gsap.fromTo(
+        ".concept-first .column-left",
+        2,
+        {
+            x: -100,
+            opacity: 0
+        },
+        {
+            x: 0,
+            opacity: 1,
+            scrollTrigger:{
+                trigger: ".concept-first .column-left",
+                start: "top center",
+                markers: true,
+            }
+        }
+    )
+    
+    gsap.fromTo(
+        ".concept-first .column-right",
+        2,
+        {
+            x: 100,
+            opacity: 0
+        },
+        {
+            x: 0,
+            opacity: 1,
+            delay: 0.5,
+            scrollTrigger:{
+                trigger: ".concept-first .column-right",
+                start: "top center",
+                markers: true,
+            }
+        }
+    )
+    
+    gsap.fromTo(
+    ".concept-second .column-left",
+    2,
+    {
+        x: -100,
+        opacity: 0
+    },
+    {
+        x: 0,
+        opacity: 1,
+        scrollTrigger:{
+            trigger: ".concept-secomd .column-left",
+            start: "top center",
+            markers: true,
+        }
+    }
+)
+
+    gsap.fromTo(
+        ".concept-second .column-right",
+        2,
+        {
+            x: 100,
+            opacity: 0
+        },
+        {
+            x: 0,
+            opacity: 1,
+            delay: 0.5,
+            scrollTrigger:{
+                trigger: ".concept-second .column-right",
+                start: "top center",
+                markers: true,
+            }
+        }
+    )
+
+
+})
+// 端末の画面幅が599px以下の時(sp)
+mm.add("(max-width: 599px)",()=> {
+    gsap.fromTo(
+        ".concept-first .column-left",
+        2,
+        {
+            y: 100,
+            opacity: 0
+        },
+        {
+            y: 0,
+            opacity: 1,
+            scrollTrigger:{
+                trigger: ".concept-first .column-left",
+                start: "top center",
+                markers: true,
+            }
+        }
+    )
+    
+    gsap.fromTo(
+        ".concept-first .column-right",
+        2,
+        {
+            y: 100,
+            opacity: 0
+        },
+        {
+            y: 0,
+            opacity: 1,
+            scrollTrigger:{
+                trigger: ".concept-first .column-right",
+                start: "top center",
+                markers: true,
+            }
+        }
+    )
+
+})
